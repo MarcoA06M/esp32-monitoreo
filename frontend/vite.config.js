951875ea541a38d2d25742d5a547b665df485a4a
Marco Marquez <mmarquez@lycee.local>
2025-08-18 00:00:16 -0600

    fix: configure base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', 
   server: {
     port: 5173,
-    host: true,
+    host: true, 
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
