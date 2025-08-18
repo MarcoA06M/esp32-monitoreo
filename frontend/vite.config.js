debcaea6927a8ba8e036c1584bc79616cb6d24f0
Marco Marquez <mmarquez@lycee.local>
2025-08-18 01:00:19 -0600

    fix: deploy frontend to cloud storage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/', 
   server: {
     port: 5173,
     host: true, 
@@ -19,6 +19,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
